3b70991e25836cf75032dce9c3b7b5a4c5829b6b
amaan <amaanshaikh20019@gmail.com>
2026-05-18 23:51:22 +0530

    color

---
+++ colorChanger/script.js
@@ -0,0 +1,18 @@
+let body = document.querySelector("body");
+let buttons = document.querySelectorAll(".button");
+
+buttons.forEach(function (button) {
+  button.addEventListener("click", function (e) {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === "grey") {
+      body.style.backgroundColor = "grey";
+    } else if (e.target.id === "white") {
+      body.style.backgroundColor = "white";
+    } else if (e.target.id === "blue") {
+      body.style.backgroundColor = "blue";
+    } else if (e.target.id === "yellow") {
+      body.style.backgroundColor = "yellow";
+    }
+  });
+});
